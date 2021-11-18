6f1302a706ce1d71bb0e9e57b43c478e43533004
David Reilly <greyblue9@users.noreply.github.com>
2021-11-18 07:51:00 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,23 @@ allprojects {
     }
 }
 
+signingConfigs {
+        release {
+            def tmpFilePath = System.getProperty("user.home") + "/work/_temp/keystore/"
+            def allFilesFromDir = new File(tmpFilePath).listFiles()
+
+            if (allFilesFromDir != null) {
+                def keystoreFile = allFilesFromDir.first()
+                keystoreFile.renameTo("keystore/your_keystore.jks")
+            }
+
+            storeFile = file("keystore/your_keystore.jks")
+            storePassword System.getenv("SIGNING_STORE_PASSWORD", "changeit")
+            keyAlias System.getenv("SIGNING_KEY_ALIAS", "example.com")
+            keyPassword System.getenv("SIGNING_KEY_PASSWORD", "changeit")
+        }
+}
+
 task("clean", type = Delete::class) {
     delete(rootProject.buildDir)
 }
